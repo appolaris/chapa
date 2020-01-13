0d688b8b40d2515bc2e068e0bc5bac49ce598291
Andrey <appolaris@gmail.com>
2020-01-13 03:57:27 +0300

    Better formatting for edge

--- src/main/scala/chapa/Edge.scala
+++ src/main/scala/chapa/Edge.scala
@@ -13,11 +13,14 @@ case class TerminalEdge(start: Int, terminal: Terminal[_]) extends Edge {
 //  override val isActive: Boolean = false
   override def symbol: Symbol[_] = terminal
   override def unprocessed: Vector[Symbol[_]] = Vector()
+  override def toString: String = s"$start, $end, $symbol •"
 }
 
 case class RuleEdge(start: Int, end: Int, rule: Rule, children: Vector[Edge], unprocessed: Vector[Symbol[_]]) extends Edge {
 //  override lazy val isActive: Boolean = children.length < rule.rhs.length
   override def symbol: Symbol[_] = rule.lhs
-//  lazy val nextSymbol: Symbol[_] = rule.rhs(children.length)
-//  val unprocessded: List[Symbol[_]] = ???
+
+  //  lazy val nextSymbol: Symbol[_] = rule.rhs(children.length)
+  //  val unprocessded: List[Symbol[_]] = ???
+  override def toString: String = s"$start, $end, ${rule.lhs} => ${children.map(_.symbol).mkString(" ")} • ${unprocessed.mkString(" ")}"
 }
