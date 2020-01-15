0d30931561032f41ec50b66da7a794a1862e7223
Andrey <appolaris@gmail.com>
2020-01-16 00:48:10 +0300

    Proto for combining algo (no actual rules).

--- src/main/scala/chapa/Parser2.scala
+++ src/main/scala/chapa/Parser2.scala
@@ -3,61 +3,28 @@ package chapa
 import scala.annotation.tailrec
 
 object Parser2 {
-  def parse(grammar: Grammar, sentence: Seq[Terminal[_]]): Set[Edge] = {
-    val initialAgenda = initBottomUpAgenda(grammar, sentence)
-    val initialChart = Set[Edge]()
-    val chart = step(grammar, chartRules, initialAgenda, initialChart)
-    // todo: group to ASTs
-    chart
-  }
+  case class State(agenda: List[Set[Edge]], result: Set[Set[Edge]]) // TODO: better name?
 
-  def initBottomUpAgenda(grammar: Grammar, sentence: Seq[Terminal[_]]): List[Edge] = {
-    sentence.zipWithIndex.map {
-      case (terminal, index) => TerminalEdge(index, terminal)
-    }.toList
-  }
-
-  // generates new edges to be added to agenda
-  // todo - probably pass this function into the loop to configure algorithm
-  def chartRules(grammar: Grammar, edge: Edge, chart: Set[Edge]): List[Edge] = {
-    // Fundamental rule
-    val filled = edge match {
-      case inactiveEdge if inactiveEdge.unprocessed.isEmpty =>
-        // Edge is inactive - find all ACTIVE edges which can be (partially?) filled by this INACTIVE edge
-        chart.collect {
-          case RuleEdge(start, end, rule, processed, nextSymbol +: unprocessed) if end == edge.start && nextSymbol == edge.symbol =>
-            RuleEdge(start, edge.end, rule, processed :+ edge, unprocessed)
-        }
-      case activeEdge: RuleEdge if activeEdge.unprocessed.nonEmpty =>
-        // Edge is ACTIVE - find all INACTIVE edges which can complete this ACTIVE edge
-        chart.collect {
-          case inactiveEdge if inactiveEdge.unprocessed.isEmpty && activeEdge.end == inactiveEdge.start && activeEdge.unprocessed.head == inactiveEdge.symbol =>
-            RuleEdge(activeEdge.start, inactiveEdge.end, activeEdge.rule , activeEdge.children :+ inactiveEdge, activeEdge.unprocessed.tail)
-        }
-    }
-
-    // Prediction rule for INACTIVE edge only - add active edges for all rules which have RHS beginning with symbol of this INACTIVE edge
-    val predicted = if (edge.unprocessed.isEmpty) {
-      grammar.rules.filter(r => r.rhs.head == edge.symbol).map(r =>
-        RuleEdge(edge.start, edge.end, r, Vector(edge), r.rhs.tail)
-      )
-    }
-    else
-      Nil
-
-    filled.toList ++ predicted
+  /** Builds compLete trees (starting from 5) by combining and extending (with "holes") set of inactive edges.
+    * @param chart Inactive edges obtained by the normal, bottom-up chart parsing. Only top Level edges are expected.
+    * @return Complete trees (starting from 5) obtained by applying rules to combine & extend subtrees.
+    */
+  def combine(chart: Set[Edge]): Set[Set[Edge]] = {
+    combineInt(State(List(chart), Set()))
   }
 
   @tailrec
-  private final def step(grammar: Grammar,
-                         chartRules: (Grammar, Edge, Set[Edge]) => List[Edge],
-                         agenda: List[Edge],
-                         chart: Set[Edge]): Set[Edge] = agenda match {
-    case Nil => chart
-    case edge::edges =>
-      val newEdges = chartRules(grammar, edge, chart)
-      val newAgenda = edges ++ newEdges // TODO: dfs vs bfs ?
-      val newChart = chart + edge
-      step(grammar, chartRules, newAgenda, newChart)
+  def combineInt(state: State): Set[Set[Edge]] = state.agenda match {
+    case chart::tail =>
+      val newState = combineAtomic(chart)
+      combineInt(State(newState.agenda ++ tail, newState.result ++ state.result))
+    case Nil => state.result
   }
+
+  /** Takes incomplete chart (set of edges/subtrees) and tries to make a more complete chart or a final tree rooted at S.
+    * How it is done depends on particular rules.
+    * @param chart Set of inactive rules to be combined / extended.
+    * @return New incomplete charts or final, results (S edges).
+    */
+  def combineAtomic(chart: Set[Edge]): State = ???
 }
