489f2916ff5c83f0797180825fa551a16ead3d65
Andrey <appolaris@gmail.com>
2020-01-21 00:47:29 +0300

    Temporarily disable recursion in grammar (not supported yet)

--- src/test/scala/chapa/TestGrammar.scala
+++ src/test/scala/chapa/TestGrammar.scala
@@ -14,9 +14,9 @@ object TestGrammar extends Grammar {
     Rule(FILTER, B_EXPR, (bExpr: BExpr) => FilterExpr(bExpr)),
 
     Rule(B_EXPR, COND, (cond: CondExpr) => cond),
-    Rule(B_EXPR, AND, (and: AndExpr) => and),
+//    Rule(B_EXPR, AND, (and: AndExpr) => and),
 
-    Rule(AND, B_EXPR, WORD("and"), B_EXPR, (expr1: BExpr, _: StringExpr, expr2: BExpr) => AndExpr(expr1, expr2)),
+//    Rule(AND, B_EXPR, WORD("and"), B_EXPR, (expr1: BExpr, _: StringExpr, expr2: BExpr) => AndExpr(expr1, expr2)),
 
     Rule(COND, Dim, Entity, (dim: DimExpr, ent: EntityExpr) => CondExpr(dim, ent)),
   )
