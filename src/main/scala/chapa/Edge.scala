6b584ec443d73479894d531cd4c203773527963f
Andrey <appolaris@gmail.com>
2020-01-20 15:56:42 +0300

    ANSI colors, just for fun (probably to be removed later)

--- src/main/scala/chapa/Edge.scala
+++ src/main/scala/chapa/Edge.scala
@@ -1,5 +1,7 @@
 package chapa
 
+import io.AnsiColor._
+
 sealed trait Edge {
   def start: Int
   def end: Int
@@ -18,5 +20,5 @@ case class TerminalEdge(start: Int, terminal: Terminal[_]) extends Edge {
 
 case class RuleEdge(start: Int, end: Int, rule: Rule, children: Vector[Option[Edge]]) extends Edge {
   override def symbol: Symbol[_] = rule.lhs
-  override def toString: String = s"""$start, $end, ${rule.lhs} => ${rule.rhs.zip(children).map(x => if (x._2.isDefined) x._1 else s"[${x._1}]").mkString(" ")}"""
+  override def toString: String = s"""$start, $end, ${rule.lhs} => ${rule.rhs.zip(children).map(x => if (x._2.isDefined) x._1 else s"$RED${x._1}$RESET").mkString(" ")}"""
 }
